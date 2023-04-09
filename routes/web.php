3d42819300592960dbc7322485acc075403b3c1a
Adeshina Ayomide <79007682+sam778-del@users.noreply.github.com>
2023-04-09 12:03:49 +0100

    authenticaiton sceffold

--- routes/web.php
+++ routes/web.php
@@ -29,3 +29,7 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 require __DIR__.'/auth.php';
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
